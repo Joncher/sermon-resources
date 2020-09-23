01a6a6770ceff3cfaf0d891477822053b3cd3e2c
John Oncher <johnoncher@gmail.com>
2020-09-23 09:44:02 -0400

    changed social media info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,8 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/nlfnyc/">@nlfnyc</a>
     </h2>
     <br />
-    <footer className="footer">
-      <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
-      </div>
-    </footer>
   </div>
 )
